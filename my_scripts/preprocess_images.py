f5eaf9aadee3ebe5af7758bab96398b40262dceb
farooq-azam-khan <farooq1.khan@ryerson.ca>
2021-10-24 20:06:12 -0400

    performed sklearn w/ KNN, need to go more advanced with keras and tensorflow

--- my_scripts/preprocess_images.py
+++ my_scripts/preprocess_images.py
@@ -23,7 +23,7 @@ int_to_label_mapping = {
     '6': 'trash'
 }
 
-def load_image_files(assertion=False):
+def load_image_files(train_data_path, test_data_path, assertion=False):
     train_data_files = list(train_data_path.glob('*.jpg'))
     test_data_files = list(test_data_path.glob('*.jpg'))
     if assertion:
@@ -35,25 +35,18 @@ def flatten_and_normalize(img):
     return img.flatten() / 255.0
 
 def grey_scale_image(img):
-    return img.mean(axis=2)
-    
-def preprocess_data(image_files, assertion=False):
+    return np.dot(img[...,:3], [0.2989, 0.5870, 0.1140])#img.mean(axis=2)
+
+def preprocess_data(image_files, col_dim, use_grey_scale):
     
    
-    if assertion: 
-        for train_data_file in tqdm(train_data_files):
-            assert orig_size == image.imread(train_data_file).shape
-            
-        for train_data_file in tqdm(train_data_files):
-            assert orig_size == image.imread(train_data_file).shape
-    
-
-    col_dim = img0.flatten().shape[0]
-
     X = np.zeros((len(image_files),col_dim))
     y = np.zeros((len(image_files),))
     for example_idx, image_file in enumerate(image_files):
-        img_data = flatten_and_normalize(image.imread(image_file))
+        if use_grey_scale: 
+            img_data = flatten_and_normalize(grey_scale_image(image.imread(image_file)))
+        else:
+            img_data = flatten_and_normalize(image.imread(image_file))
         X[example_idx] = img_data 
         if 'cardboard' in str(image_file):
             y[example_idx] = label_to_int_mapping['cardboard']
@@ -69,12 +62,26 @@ def preprocess_data(image_files, assertion=False):
             y[example_idx] = label_to_int_mapping['trash']
     return X, y
     
+def get_preprocessed_train_test_images(use_grey_scale=True, assertion=False):
+    base = pathlib.Path('../data')
+    train_data_path = base / 'train'
+    test_data_path = base / 'test'
+    train_data_files, test_data_files = load_image_files(train_data_path, test_data_path)
+    img0 = image.imread(train_data_files[0])
+    orig_size = image.imread(train_data_files[0]).shape
 
-base = pathlib.Path('../data')
-train_data_path = base / 'train'
-test_data_path = base / 'test'
-train_data_files, test_data_files = load_image_files()
-img0 = image.imread(train_data_files[0])
-orig_size = image.imread(train_data_files[0]).shape
-X_train, y_train = preprocess_data(train_data_files)
-X_test, y_test = preprocess_data(test_data_files)
+    if assertion: 
+        for train_data_file in tqdm(train_data_files):
+            assert orig_size == image.imread(train_data_file).shape
+            
+        for train_data_file in tqdm(train_data_files):
+            assert orig_size == image.imread(train_data_file).shape
+
+    if use_grey_scale: 
+        col_dim = grey_scale_image(img0).flatten().shape[0]
+    else: 
+        col_dim = img0.flatten().shape[0]
+    print(grey_scale_image(img0).shape)
+    X_train, y_train = preprocess_data(train_data_files, col_dim, use_grey_scale)
+    X_test, y_test = preprocess_data(test_data_files, col_dim, use_grey_scale)
+    return X_train, X_test, y_train, y_test
