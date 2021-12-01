b1f6181836eba2e653dabfe8d4b3ce1db17ab2ae
farooq-azam-khan <farooq1.khan@ryerson.ca>
2021-12-01 14:58:10 -0500

    refactoring evaluation code

--- my_scripts/evaluate_model.py
+++ my_scripts/evaluate_model.py
@@ -1,8 +1,13 @@
+import os 
+import argparse
+
+import numpy as np 
+from sklearn import metrics 
+
 from tensorflow import keras
 import tensorflow as tf
-import numpy as np 
 
-import argparse
+from helpers import plot_confusion_matrix
 
 def get_parser():
     parser = argparse.ArgumentParser(description='Evaluate the model')
@@ -12,22 +17,6 @@ def get_parser():
                     help='What model is it (NN, CNN, Resnet, EfficientNet)?')
 
     return parser 
-
-args = get_parser().parse_args()
-
-import os 
-if not os.path.isfile(args.model_dir):
-    raise ValueError(f'Model directory {args.model_dir} does not exist')
-
-print(f'Loading Model from {args.model_dir}')
-
-model_type = args.model_type
-if not os.path.exists(f'../my_scripts/plots/{model_type}'):
-    # os.mkdir(f'../my_scripts/plots/{model_type}')
-    raise ValueError(f'{model_type} dir does not exist. Have you trained it?')
-    
-model_dir = args.model_dir# #'../my_scripts/models/EfficientNet/en_dense_74_0.90.h5'
-model = keras.models.load_model(model_dir)
 
 
 
@@ -49,26 +38,37 @@ def load_test_data():
     )
     return test_ds
 
-    # pass 
-
-def main():
+def evaluate_model_on_test_data(model, model_type: str, time_stamp: int):
     test_ds = load_test_data()
     predictions = model.predict(test_ds)
     test_loss, test_acc = model.evaluate(test_ds)
 
     one_hot_labels = np.concatenate([y for x,y in test_ds])
 
-    from sklearn import metrics 
     cm = metrics.confusion_matrix(y_true=one_hot_labels.argmax(axis=1), y_pred=predictions.argmax(axis=1))#, labels=target_classes)
     print(cm)
+    
+    # plot confusion matrix
+    plot_confusion_matrix(cm, test_loss, test_acc, classes=target_classes, time_stamp=time_stamp, model_type=model_type)
 
-    from matplotlib import pyplot as plt
+def main():
+    
+    args = get_parser().parse_args()
+
+    if not os.path.isfile(args.model_dir):
+        raise ValueError(f'Model directory {args.model_dir} does not exist')
+
+    print(f'Loading Model from {args.model_dir}')
+
+    model_type = args.model_type
+    if not os.path.exists(f'../my_scripts/plots/{model_type}'):
+        # os.mkdir(f'../my_scripts/plots/{model_type}')
+        raise ValueError(f'{model_type} dir does not exist. Have you trained it?')
+        
+    model_dir = args.model_dir# #'../my_scripts/models/EfficientNet/en_dense_74_0.90.h5'
+    model = keras.models.load_model(model_dir)
     import time 
-    f = plt.figure(figsize=(10,7))
-    ax = f.add_subplot(111)
-    plt.title(f'{model_type} Model Confusion Matrix - Loss: {test_loss:.2f} - Test Acc: {test_acc:.2f}')
-    metrics.ConfusionMatrixDisplay.from_predictions(y_true=one_hot_labels.argmax(axis=1), y_pred=predictions.argmax(axis=1), display_labels=target_classes,cmap='magma', ax=ax, colorbar=False)
-    plt.savefig(f'../my_scripts/plots/{model_type}/{int(time.time())}_confusion_matrix.jpg')
+    evaluate_model_on_test_data(model, model_type, time_stamp=int(time.time()))
 
 
 if __name__ == '__main__':
--- my_scripts/helpers.py
+++ my_scripts/helpers.py
@@ -1,6 +1,12 @@
 import tensorflow as tf 
 import pathlib 
 import matplotlib.pyplot as plt
+import itertools
+import numpy as np
+from matplotlib import style
+import tikzplotlib
+
+style.use('ggplot')
 
 def get_plot_file_name(model_type, plot_type, time_stamp, image_size):
     """
@@ -38,7 +44,7 @@ def load_train_val_data(image_size=(384, 512), batch_size=16):
     return train_ds, val_ds
 
 
-def plot_loss(history, time_stamp, batch_size, image_size, model_type, epochs):
+def plot_loss(history, time_stamp, batch_size, image_size, model_type, epochs, save_as_tex=True):
     plt.figure()
     plt.title(f'Simple NN Loss - batch size {batch_size}')
     plt.xlabel('Epoch')
@@ -48,9 +54,12 @@ def plot_loss(history, time_stamp, batch_size, image_size, model_type, epochs):
     plt.legend(['Validation Loss', 'Training Loss'])
     
     plt.savefig(f'./plots/{model_type}/{time_stamp}_loss_size_{image_size[0]}_{image_size[1]}.jpeg')
+    if save_as_tex:
+        tikzplotlib.save(f'./plots/{model_type}/{time_stamp}_loss_size_{image_size[0]}_{image_size[1]}.tex')
+    plt.close()
 
 
-def plot_accuracy(history, time_stamp, batch_size, image_size, model_type, epochs):
+def plot_accuracy(history, time_stamp, batch_size, image_size, model_type, epochs, save_as_tex=True):
     plt.figure()
     plt.title(f'Simple NN Accuracy - batch size {batch_size}')
     plt.xlabel('Epoch')
@@ -59,3 +68,49 @@ def plot_accuracy(history, time_stamp, batch_size, image_size, model_type, epoch
     plt.plot(range(1, epochs+1), history.history['accuracy'])
     plt.legend(['Validation Accuracy', 'Training Accuracy'])
     plt.savefig(f'./plots/{model_type}/{time_stamp}_acc_size_{image_size[0]}_{image_size[1]}.jpeg')
+    if save_as_tex:
+        tikzplotlib.save(f'./plots/{model_type}/{time_stamp}_acc_size_{image_size[0]}_{image_size[1]}.tex')
+    plt.close()
+
+
+def plot_confusion_matrix(cm, test_loss, test_acc, classes, time_stamp, model_type, normalize=False, save_as_tex=True):
+    plt.title(f'{model_type} Model Confusion Matrix - Loss: {test_loss:.2f} - Test Acc: {test_acc:.2f}')
+
+    accuracy = np.trace(cm) / float(np.sum(cm))
+    misclass = 1 - accuracy
+    cmap = plt.get_cmap('Blues')
+
+    plt.figure(figsize=(8, 6))
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(f'{model_type} Confusion Matrix')
+    plt.colorbar()
+
+    if classes is not None:
+        tick_marks = np.arange(len(classes))
+        plt.xticks(tick_marks, classes, rotation=45)
+        plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+
+    thresh = cm.max() / 1.5 if normalize else cm.max() / 2
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        if normalize:
+            plt.text(j, i, "{:0.4f}".format(cm[i, j]),
+                    horizontalalignment="center",
+                    color="white" if cm[i, j] > thresh else "black")
+        else:
+            plt.text(j, i, "{:,}".format(cm[i, j]),
+                    horizontalalignment="center",
+                    color="white" if cm[i, j] > thresh else "black")
+
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel(f'Predicted label\nAccuracy: {test_acc:.2f} - LossS: {test_loss:.2f} - Misclassification: {misclass:.2f}')
+    # plt.show()
+    plt.savefig(f'./plots/{model_type}/{time_stamp}_confusion_matrix_size.jpeg')
+    if save_as_tex:
+        tikzplotlib.save(f'./plots/{model_type}/{time_stamp}_confusion_matrix_size.tex')
+    plt.close()
